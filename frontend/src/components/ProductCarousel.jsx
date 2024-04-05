e870f5ba7fefb44b1f8d4d809668b143afe665d0
R.V.SARISH <21it210@stjosephs.ac.in>
2024-04-05 11:44:52 +0530

    3

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Carousel, Image } from 'react-bootstrap';
 import Message from './Message';
@@ -5,24 +6,46 @@ import { useGetTopProductsQuery } from '../slices/productsApiSlice';
 
 const ProductCarousel = () => {
   const { data: products, isLoading, error } = useGetTopProductsQuery();
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) =>
+        prevIndex === products.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [products]);
 
   return isLoading ? null : error ? (
     <Message variant='danger'>{error?.data?.message || error.error}</Message>
   ) : (
-    <Carousel pause='hover' className='bg-primary mb-4'>
-      {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className='carousel-caption'>
-              <h2 className='text-white text-right'>
-                {product.name} ₹{product.price})
-              </h2>
-            </Carousel.Caption>
-          </Link>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <div style={{ position: 'relative', width: '100%', overflow: 'hidden' }}>
+      <Carousel
+        activeIndex={index}
+        onSelect={() => {}}
+        pause={false}
+        className='bg-primary mb-4'
+      >
+        {products.map((product) => (
+          <Carousel.Item key={product._id}>
+            <Link to={`/product/${product._id}`}>
+              <Image
+                src={product.image}
+                alt={product.name}
+                fluid
+                style={{ width: '400px', height: '300px' }}
+              />
+              <Carousel.Caption className='carousel-caption'>
+                <h2 className='text-white text-right'>
+                  {product.name} ₹{product.price}
+                </h2>
+              </Carousel.Caption>
+            </Link>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 };
 
